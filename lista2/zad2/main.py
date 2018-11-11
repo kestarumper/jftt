0b7e336d748b3ad06e61260b6784887545ac3924
Adrian Mucha <kestarumper8@gmail.com>
2018-11-11 20:54:01 +0100

    add highlight color

--- lista2/zad2/main.py
+++ lista2/zad2/main.py
@@ -1,14 +1,17 @@
 import ply.lex as lex
 import sys
 
-tokens = ['COMMENT', 'NEWLINE', 'HTMLTAG', 'INNERHTML']
+tokens = ['COMMENT', 'NEWLINE', 'HTMLTAG', 'TEXT']
 
 t_NEWLINE = r'\n+'
 t_HTMLTAG = r'</?[a-zA-Z]+\s*.*/?>'
-t_INNERHTML = r'\"|\'|.+'
+t_TEXT = r'\"|\'|.+'
 
 def t_COMMENT(t):
     r'<!--(.|\n|\/)*?-->\n*'
+    print("\033[93m", file=sys.stderr, end='')
+    print(t, file=sys.stderr)
+    print("\033[0m", file=sys.stderr, end='')
     # return t
 
 def t_error(t):
